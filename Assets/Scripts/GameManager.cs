6e0fd3d22622ab5373e4c4e94a0bc1ec51cafcda
William Suwandi <ws2315@nyu.edu>
2022-10-09 16:21:45 -0400

    Words

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,20 +21,46 @@ public class GameManager : MonoBehaviour
 
     public GameObject Akira;
     public GameObject Allison;
+    public GameObject Adrien;
     public GameObject choiceOne;
     public GameObject choiceTwo;
+    public GameObject choiceThree;
     public GameObject nextButton;
+
+    public TextMeshProUGUI choiceOneText;
+    public TextMeshProUGUI choiceTwoText;
+    public TextMeshProUGUI choiceThreeText;
 
     // Start is called before the first frame update
     void Start()
     {
         choiceOne.SetActive(false);
         choiceTwo.SetActive(false);
+        choiceThree.SetActive(false);
         Akira.SetActive(false);
         Allison.SetActive(false);
+        Adrien.SetActive(false);
 
         currentDialogue = phaseZeroDialogue;
         dialogueBox.text = currentDialogue[dialogueIndex];
+
+        choiceOneText = choiceOne.GetComponentInChildren<TextMeshProUGUI>();
+        choiceTwoText = choiceTwo.GetComponentInChildren<TextMeshProUGUI>();
+        choiceThreeText = choiceThree.GetComponentInChildren<TextMeshProUGUI>();
+        choiceSetWord();
+    }
+
+    public void choiceSetWord()
+    {
+        switch (phaseIndex)
+        {
+            case 0:
+                choiceTwoText.text = "Continue";
+                break;
+            case 1:
+                choiceTwoText.text = "Forward";
+                break;
+        }
     }
 
     void SetDialogueText()
@@ -64,10 +90,34 @@ public class GameManager : MonoBehaviour
 
     void SetupChoices()
     {
-        //turn off the next button and turn on the choice buttons
         nextButton.SetActive(false);
-        choiceOne.SetActive(true);
-        choiceTwo.SetActive(true);
+        if (phaseIndex != 0)
+        {
+            choiceOne.SetActive(true);
+            choiceTwo.SetActive(true);
+        } else
+        {
+            choiceOne.SetActive(false);
+            choiceTwo.SetActive(false);
+        }
+        choiceThree.SetActive(true);
+    }
+
+    public void CorrectChoice()
+    {
+        correctpath++;
+        GoToNextPhase();
+    }
+
+    public void NeutralChoice()
+    {
+        GoToNextPhase();
+    }
+
+    public void WrongChoice()
+    {
+        correctpath--;
+        GoToNextPhase();
     }
 
     void GoToNextPhase()
@@ -76,6 +126,7 @@ public class GameManager : MonoBehaviour
         nextButton.SetActive(true);
         choiceOne.SetActive(false);
         choiceTwo.SetActive(false);
+        choiceThree.SetActive(false);
         dialogueIndex = 0;
         switch (phaseIndex)
         {
@@ -84,22 +135,27 @@ public class GameManager : MonoBehaviour
                 Akira.SetActive(true);
                 Allison.SetActive(true);
                 phaseIndex = 1;
+                choiceSetWord();
                 break;
             case 1:
                 currentDialogue = phaseTwoDialogue;
                 phaseIndex = 2;
+                choiceSetWord();
                 break;
             case 2:
                 currentDialogue = phaseThreeDialogue;
                 phaseIndex = 3;
+                choiceSetWord();
                 break;
             case 3:
                 currentDialogue = phaseFourDialogue;
                 phaseIndex = 4;
+                choiceSetWord();
                 break;
             case 4:
                 currentDialogue = phaseFiveDialogue;
                 phaseIndex = 5;
+                choiceSetWord();
                 break;
         }
         SetDialogueText();
